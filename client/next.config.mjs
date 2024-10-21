2a5ae77f92685751aa418d9d80b5e90879e6b70d
Nikita Kurochka <freenikita95@gmail.com>
2024-10-20 17:16:45 -0700

    add client

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,15 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "tailwindui.com",
-            },
-        ],
-        deviceSizes: [360, 414, 768, 1366, 1536, 1920],
-        imageSizes: [40, 58, 140, 284, 484, 968],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com"
+      }
+    ],
+    deviceSizes: [360, 414, 768, 1366, 1536, 1920],
+    imageSizes: [40, 58, 140, 284, 484, 968]
+  }
 };
 
 export default nextConfig;
