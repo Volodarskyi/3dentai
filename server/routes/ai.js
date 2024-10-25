ed86c74c373f83df34c452af9c3d936eafa8736e
Nikita Kurochka <freenikita95@gmail.com>
2024-10-25 15:50:52 -0700

    connect API to mistral and refactoring

---
+++ server/routes/ai.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import aiController from '../controllers/ai.js';
+
+const router = Router();
+
+router.post('/analyze', aiController.analyzeImages);
+
+export default router;
