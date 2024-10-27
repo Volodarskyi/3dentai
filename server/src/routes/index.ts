84f7019f6dd0497c099197b54443b82a2f1168fa
Volodarskyi <Volodarskyi@users.noreply.github.com>
2024-10-27 02:12:13 -0700

    [ADD] bycrypt. change to ts for server

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import aiRout from './ai';
 import photoRout from './photo';
 import { generateRes } from '../utils/api';
+import authRoutes from "./authRoutes";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use('/ai', aiRout);
 app.use('/photo', photoRout);
+app.use('/auth', authRoutes);
 
 // Default
 app.use((req: Request, res: Response) => {
