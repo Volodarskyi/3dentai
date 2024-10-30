5517fcacbfca30d78d062f41287cd4a46111a74a
Volodarskyi <Volodarskyi@users.noreply.github.com>
2024-10-29 21:13:30 -0700

    Fix Eslint

--- client/src/store/reducers/user-store.ts
+++ client/src/store/reducers/user-store.ts
@@ -1,51 +1,52 @@
-import { makeAutoObservable } from 'mobx';
-import {jwtDecode} from "@/helper/parseFunctions";
-import {EAuth} from "@/types/auth";
+import { makeAutoObservable } from "mobx";
+
+import { jwtDecode } from "@/helper/parseFunctions";
+import { EAuth } from "@/types/auth";
 export class UserStore {
-    isAuth = false;
-    firstName = ''
-    secondName =''
-    role = ''
+  isAuth = false;
+  firstName = "";
+  secondName = "";
+  role = "";
 
-    constructor() {
-        makeAutoObservable(this);
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setAuth = (isAuth: boolean) => {
+    this.isAuth = isAuth;
+  };
+
+  setFirstName = (firstName: string) => {
+    this.firstName = firstName;
+  };
+
+  setSecondName = (secondName: string) => {
+    this.secondName = secondName;
+  };
+
+  setRole = (role: string) => {
+    this.role = role;
+  };
+
+  authorization = () => {
+    const localToken = localStorage.getItem(EAuth.TOKEN_ITEM_NAME);
+
+    if (localToken) {
+      const tokenData = jwtDecode(localToken);
+
+      this.setFirstName(tokenData.payload.first);
+      this.setSecondName(tokenData.payload.second);
+      this.setRole(tokenData.payload.role);
+      this.setAuth(true);
     }
+  };
 
-    setAuth = (isAuth:boolean)=>{
-        this.isAuth = isAuth;
-    }
-
-    setFirstName = (firstName:string)=>{
-        this.firstName = firstName
-    }
-
-    setSecondName = (secondName:string)=>{
-        this.secondName = secondName
-    }
-
-    setRole = (role:string)=>{
-        this.role = role
-    }
-
-    authorization = ()=>{
-        const localToken = localStorage.getItem(EAuth.TOKEN_ITEM_NAME);
-
-        if (localToken) {
-            const tokenData = jwtDecode(localToken)
-
-            this.setFirstName(tokenData.payload.first)
-            this.setSecondName(tokenData.payload.second)
-            this.setRole(tokenData.payload.role)
-            this.setAuth(true)
-        }
-    }
-
-    logout = ()=>{
-        console.log('auth-logout')
-        this.setFirstName('')
-        this.setSecondName('')
-        this.setRole('')
-        this.setAuth(false)
-        localStorage.removeItem(EAuth.TOKEN_ITEM_NAME)
-    }
+  logout = () => {
+    console.log("auth-logout");
+    this.setFirstName("");
+    this.setSecondName("");
+    this.setRole("");
+    this.setAuth(false);
+    localStorage.removeItem(EAuth.TOKEN_ITEM_NAME);
+  };
 }
