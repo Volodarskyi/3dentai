35bfac9369f9eddd277b748d578d9abc8b7f3f0d
PasichnykOleksa <pasichnykoleksa@gmail.com>
2024-11-08 12:46:10 -0800

    updated to the infra

--- infrastructure/bootstrap/terraform/instances.tf
+++ infrastructure/bootstrap/terraform/instances.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "app_instance" {
   subnet_id                   = data.aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
-  key_name                    = "jenkins_hackaton_3denai" # Replace with your SSH key name
+  key_name                    = "jenkins-ansible" # Replace with your SSH key name
 
   root_block_device {
     volume_size = 8
